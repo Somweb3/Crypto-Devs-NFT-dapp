0de7b839665bf46cba9e4e766264f4665fff2413
Somweb3 <msomnath038@gmail.com>
2022-12-23 11:03:22 +0530

    second commit

--- Contract/constants/index.js
+++ Contract/constants/index.js
@@ -1,5 +1,5 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x0531557a7105aCFd0755e543a91CC5f7654E4a2e";
 
-const METADATA_URL = "whitelist-daap-pthsyr013-somweb3.vercel.app";
+const METADATA_URL = "nft-collection-dag3viucg-somweb3.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
