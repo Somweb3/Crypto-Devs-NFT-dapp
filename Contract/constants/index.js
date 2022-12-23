71c37da9995de99ffefc59f43eb62d091f74c212
Somweb3 <msomnath038@gmail.com>
2022-12-23 09:42:00 +0530

    first commit

---
+++ Contract/constants/index.js
@@ -0,0 +1,5 @@
+const WHITELIST_CONTRACT_ADDRESS = "0x0531557a7105aCFd0755e543a91CC5f7654E4a2e";
+
+const METADATA_URL = "whitelist-daap-pthsyr013-somweb3.vercel.app";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
